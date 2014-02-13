81311f7a197ad1418857cf235265b2450266ba6c
U-MediaSVR\Home <Home@MediaSVR.(none)>
2014-02-13 14:29:56 +1100

    Fix leading zero handling error

--- GenMissing.sh
+++ GenMissing.sh
@@ -2,30 +2,33 @@
 
 # Pad a zero in sequel number in its files name.
 
-missing="Missong.txt"
+missing="Missing.txt"
 USAGE="Usage: $0 file-list-file"
 
 [ $# -ne 1 ] && echo "$USAGE" && exit 1
 
-nextidx=0
-
+echo "Missing files are"
+nextidx=1
 cat "$1" |
   while read fname; do
 
     [ -z "$fname" ] && continue
 
     fname=${fname/회*/}
-    seq=${fname/*제0/}
+    seq=${fname/*제/}
+
+    # strip off leading zero
+    seq=$((10#$seq))
 
     # set start index
-    (( $nextidx == 0 )) && nextidx=$seq
-    # echo "seq=$seq nextidx=$nextidx"
+    #(( $nextidx == 0 )) && nextidx=$seq
 
     # if current seq is greater than the expected
     if (( $seq > $nextidx )); then
 
       # last index
       lastidx=$(( $seq - 1 ))
+      # echo "seq=$seq nextidx=$nextidx lastidx=$lastidx"
 
       # only one file is missed?
       if (( $lastidx == $nextidx )); then
