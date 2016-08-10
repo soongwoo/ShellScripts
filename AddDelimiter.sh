128408d769c58d357086592188a66b18dee5c0c5
Soongwoo Chong <soongwoo@gmail.com>
2016-08-11 08:06:46 +1000

    finish AddDelimietr.sh

--- AddDelimiter.sh
+++ AddDelimiter.sh
@@ -3,11 +3,11 @@
 # Add a delimiter in the given n-th argument of the given filenames
 # - delimiter='.'
 
-debug=1
-PivotARG=3
+debug=0
+NthARG=3
 DELIMITER='.'
 NextArgLen=6
-OPTIONS="[-debug=0] [-DELIMITER=.] [-PivotARG=3] [-NextArgLen=6]"
+OPTIONS="[-debug=0] [-DELIMITER=.] [-NthARG=3] [-NextArgLen=6]"
 USAGE="Usage: $0 $OPTIONS file1 file2 ... fileN"
 
 [ $# -lt 1 ] && echo "$USAGE" && exit 1
@@ -35,28 +35,46 @@ while (( "$#" )); do
     fname="$1"
 
     # get arguments
-    let NextARG=PivotARG+1
-    pivotarg=$(echo "$fname" | cut -d $DELIMITER -f $PivotARG)
+    let NextARG=NthARG+1
+    ntharg=$(echo "$fname" | cut -d $DELIMITER -f $NthARG)
     nextarg=$(echo "$fname" | cut -d $DELIMITER -f $NextARG)
+    [ $debug -eq 1 ] && echo "ntharg='$ntharg' nextarg='$nextarg'"
 
-    # if nextarg's length is not equal to the given length
-    if [ ${#nextarg} -ne $NextArgLen ]; then
-    [ $debug -eq 1 ] && echo "pivotarg='$pivotarg' nextarg='$nextarg'"
+    # validate ntharg whether a delimiter can be inserted
+    ntharglen=${#ntharg}
 
-    # make the new filename by swaping date argument and name argument
-#    headstr=${fname/$pivotarg*/};
-#    tailstr=${fname/*$nextarg/};
-#    new_fname="$headstr""$nextarg""$DELIMITER""$pivotarg""$tailstr"
-#    [ $debug -eq 1 ] && echo "headstr='$headstr' tailstr='$tailstr'"
-#    [ $debug -eq 2 ] && echo "new_fname='$new_fname'"
+    # if nextarg's length is equal to the given length
+    if [ ${#nextarg} -eq $NextArgLen ]; then
+      echo "'$fname': Valid file name"
 
-    # rename it
-#    if [ -e "$new_fname" ]; then
-#      echo "'$new_fname' does exist!"
-#    else
-#      echo -e "Rename '$fname' as\n\t'$new_fname'"
-#      mv "$fname" "$new_fname"
-#    fi
+    # check whether a delimiter can be inserted
+    elif [ $ntharglen -le $NextArgLen ]; then
+      echo "'$ntharg': Invalid argument"
+
+    # insert a delimiter
+    else
+
+      # calculate delimiter position
+      let pos=ntharglen-$NextArgLen
+
+      # get strings before/after delimiter position
+      lower=${ntharg:pos}
+      upper=${ntharg/$lower/}
+
+      # make the new filename by swaping date argument and name argument
+      headstr=${fname/$ntharg*/};
+      tailstr=${fname/*$ntharg/};
+      new_fname="$headstr""$upper""$DELIMITER""$lower""$tailstr"
+      [ $debug -eq 1 ] && echo "headstr='$headstr' tailstr='$tailstr'"
+      [ $debug -eq 2 ] && echo "new_fname='$new_fname'"
+
+      # rename it
+      if [ -e "$new_fname" ]; then
+        echo "'$new_fname' does exist!"
+      else
+        echo -e "Rename '$fname' as\n\t'$new_fname'"
+        mv "$fname" "$new_fname"
+      fi
 
     fi
 
