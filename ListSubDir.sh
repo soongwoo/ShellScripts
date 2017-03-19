d99e23ad0ddebc0cb6139ab809884261e92a7b16
Soongwoo Chong <soongwoo@gmail.com>
2017-03-19 19:49:03 +1100

    Add ListSubDir.sh

---
+++ ListSubDir.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# List the subdirectories of the given directories.
+
+OPTION="[-mode=debug]"
+USAGE="Usage: $0 dir1 dir2 ... dirN"
+
+[ $# -lt 1 ] && echo "$USAGE" && exit 1
+
+curdir=$(pwd)
+
+while (( "$#" )); do
+
+  # option argument
+  if [ ${1:0:1} = '-' ]; then
+    tmp=${1:1}              # strip off leading '-'
+    parameter=${tmp%%=*}    # extract name
+    value=${tmp##*=}        # extract value
+    eval $parameter=$value
+
+  # directory name
+  elif [ -d "$1" ]; then
+
+    echo ""
+    echo "$1:"
+
+    find "$1"/* -type d | while read subdir; do
+      dname=${subdir#$1}
+      echo "  ${dname:1}"
+    done
+
+  # what is it?
+  else
+    echo ""
+    echo "$1: not a directory!"
+  fi
+
+  shift
+
+done
