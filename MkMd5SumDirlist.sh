7073f5376dd0224bbdc283b396ddba3876ae0de6
Soong Woo Chong <soongwoo@gmail.com>
2019-01-11 11:20:05 +1100

    polish codes

--- MkMd5SumDirlist.sh
+++ MkMd5SumDirlist.sh
@@ -2,8 +2,7 @@
 
 # Create a file, which has md5sum for all files in the given directory.
 
-build="yes"
-OPTION="[-build=yes|no]"
+OPTION="[-mode=debug]"
 USAGE="Usage: $0 dir1 dir2 ... dirN"
 
 [ $# -lt 1 ] && echo "$USAGE" && exit 1
@@ -22,28 +21,15 @@ while (( "$#" )); do
   # directory name
   elif [ -d "$1" ]; then
 
-    # take off '/', if it is a last character.
-    # fullname="$1"
-    # lastch=$(echo -n "$fullname" | tail -c -1)
-    # [ "$lastch" == "/" ] && fullname=${fullname/%\//}
-
-    # get directory name
-    # dirname=${fullname##.*\/}
-    dname=${1%/}
+    dname=${1%/}	# remove trailing slash char
     md5="$dname".md5
 
     # check md5sum file
     echo ""
-    if [ "$build" == "no" ]; then
-      if [ ! -f "$dname/$dname.md5" ]; then
-        echo "$dname: NO $md5"
-      else
-        echo "$dname: $md5 exists!"
-      fi
+    if [ -z "$(ls -A $dname)" ]; then
+      echo "$1: empty directory!"
     elif [ -f "$dname/$dname.md5" ]; then
       echo "$dname: $md5 exists!"
-    elif [ -z "$(ls -A $dname)" ]; then
-      echo "$1: empty directory!"
     else
       echo "$dname: Create $md5 ..."
 
@@ -55,7 +41,7 @@ while (( "$#" )); do
       done
       cd "$curdir"
 
-      [ ! -f "$dname/$dname.txt" ] && echo "Warning: No '$dname/$dname.txt'"
+      # [ ! -f "$dname/$dname.txt" ] && echo "Warning: No '$dname/$dname.txt'"
     fi
 
   # what is it?
